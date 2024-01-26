7b514dc0695a7a9c38908bde859157c463ac2a27
egee-irl <brian@egee.io>
2024-01-26 14:56:49 -0800

    fix import name

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable space-before-function-paren */
 /* eslint-disable require-jsdoc */
 import process from 'process'
-import BackupManager from './BackupManager.js'
+import BackupManager from './backupManager.js'
 
 const backupObj = new BackupManager(process.argv[2])
 backupObj.backup()
