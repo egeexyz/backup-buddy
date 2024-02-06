86055ebfa63ff0c050528d35d32e8c1f5f009465
egee-irl <brian@egee.io>
2024-02-06 08:27:19 -0800

    Update cucumber, enhance test stubs

--- tests/backup.js
+++ tests/backup.js
@@ -1,14 +1,17 @@
 const { Given, When, Then } = require('cucumber')
 
-Given('I have a backup job file', () => {
-  // Build a yaml file
+Given('I have a backup job', () => {
+  console.log('Create some dummy files to back up')
+  console.log('Generate markup for job file')
+  console.log('Create temporary job file')
 })
 
 When('I pass the job file to backup-buddy as an argument', () => {
-  // Invoke backup-buddy, pass job file in as arg
+  console.log('Save a list of the files, maybe a sha?')
+  console.log('Invoke backup-buddy, pass temp job file in as arg')
 })
 
 Then('I will see my files are backed up in the desired folder', () => {
-  // Ensure source & desired are same
-  // Clean out desired dir
+  console.log('Ensure source & desired are same')
+  console.log('Clean out desired dir')
 })
